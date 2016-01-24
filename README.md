e62c735342f4b53e2834892d76607648e61f95e0
Joel Kuntz <joel.kuntz.92@gmail.com>
2016-01-24 10:26:48 -0400

    Let default organization be set at command line

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ A slack bot to make linking and creating GitHub issues easier
 1. Create a [new slack bot](http://my.slack.com/services/new/bot)
 2. Clone this repository on your server
 3. Run the node server with `token=your_slackbot_token node issuebot.js`
+  - You can also set a default organization that will persist between server resets that doesn't depend on storage via `token=your_slackbot_token organization=affinio node issuebot.js`
 4. `/invite issuebot` to your channels/groups
 
 ## Usage
--- issuebot.js
+++ issuebot.js
@@ -13,6 +13,11 @@ var messageEvents = ["direct_message","direct_mention","mention","ambient"];
     process.exit(1);
   }
 
+  var defaultOrganization = null;
+  if (process.env.organization) {
+    defaultOrganization = process.env.organization;
+  }
+
   var Botkit = require('botkit');
 
   var controller = Botkit.slackbot({
@@ -39,28 +44,40 @@ var messageEvents = ["direct_message","direct_mention","mention","ambient"];
 
   controller.hears("issuebot (help|command)", messageEvents, function(bot, message){
     bot.reply(message, "Hi, here are the commands I understand:");
-    bot.reply(message, "You can set a default username or organization with `issuebot set default organization affinio`");
+
+    if (!defaultOrganization){
+      bot.reply(message, "You can set a default username or organization with `issuebot set default organization affinio`");
+    }
+
     bot.reply(message, "You can get an issue link by typing `organizationname/reponame#123`");
-    bot.reply(message, "If you have set a defualt organization you can use `reponame#123`");
+
+    if (!defaultOrganization){
+      bot.reply(message, "If you have set a defualt organization you can use `reponame#123`");
+    }
+    else {
+      bot.reply(message, "Your default organization is `"+defaultOrganization+"` you can use `reponame#123` directly");
+    }
   });
 //**** End bot help
 
 //**** Bot configuration
   controller.hears("issuebot set default (organization|username) ([A-Za-z0-9-]+[^-]$)", messageEvents, function(bot, message){
-    var organization = message.text.match(/issuebot set default (organization|username) ([A-Za-z0-9-]*[^-]$)/);
-    if (organization && organization.length === 3){
-      controller.storage.teams.save({id: "organization", organization:organization[2]}, function(err) {
-        if (err){
-          bot.reply(message, "There was an error please report it and/or contact your slack maintainer https://github.com/affinio/slack-issuebot/issues");
-        }
-        else {
-          bot.reply(message, "I have set your default organization to `"+organization[2]+"`\nYou can now say `reponame#123` and I will link to this organization");
-        }
-      });
-    } // if we got the proper organization info
-    else {
-      bot.reply(message, "Organization name not found. Try: `issuebot set default organization orgname`");
-    }
+    if (!defaultOrganization){
+      var organization = message.text.match(/issuebot set default (organization|username) ([A-Za-z0-9-]*[^-]$)/);
+      if (organization && organization.length === 3){
+        controller.storage.teams.save({id: "organization", organization:organization[2]}, function(err) {
+          if (err){
+            bot.reply(message, "There was an error please report it and/or contact your slack maintainer https://github.com/affinio/slack-issuebot/issues");
+          }
+          else {
+            bot.reply(message, "I have set your default organization to `"+organization[2]+"`\nYou can now say `reponame#123` and I will link to this organization");
+          }
+        });
+      } // if we got the proper organization info
+      else {
+        bot.reply(message, "Organization name not found. Try: `issuebot set default organization orgname`");
+      }
+    } // if we didn't set an organization on the command line
   });
 //**** End bot configuration
 
@@ -81,17 +98,22 @@ var messageEvents = ["direct_message","direct_mention","mention","ambient"];
   controller.hears("([A-Za-z0-9_.-]+)#([0-9]+)", messageEvents, function(bot,message) {
     var repositoryMessage = message.text.match(/([A-Za-z0-9_.-]+)#([0-9]+)/);
     if (repositoryMessage && repositoryMessage.length === 3){
-      controller.storage.teams.get("organization", function(err, data){
-        if (err){ console.log(err); }
-        if (data){
-          var reponame = repositoryMessage[1];
-          var issueNum = repositoryMessage[2];
-          bot.reply(message, "https://github.com/"+data.organization+"/"+reponame+"/issues/"+issueNum);
-        }
-        else {
-          bot.reply(message, "issue detected but no default organization/username set. You can set one with: `issuebot set default organization affinio`");
-        }
-      });
+      var reponame = repositoryMessage[1];
+      var issueNum = repositoryMessage[2];
+      if (!defaultOrganization){
+        controller.storage.teams.get("organization", function(err, data){
+          if (err){ console.log(err); }
+          if (data){
+            bot.reply(message, "https://github.com/"+data.organization+"/"+reponame+"/issues/"+issueNum);
+          }
+          else {
+            bot.reply(message, "issue detected but no default organization/username set. You can set one with: `issuebot set default organization affinio`");
+          }
+        });
+      } // no command line organization set
+      else {
+        bot.reply(message, "https://github.com/"+defaultOrganization+"/"+reponame+"/issues/"+issueNum);
+      }
     } // if we got proper repository information
     else { bot.reply(message, "Error parsing repository information"); }
   });
